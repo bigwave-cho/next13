06a4ab3ee854ae6c3b697271613fa7cef7777da4
bigwave-cho <hooyaa93@gmail.com>
2023-05-21 23:25:44 +0900

    EmailPW: JWT 생성, admin 팁

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,17 +1,20 @@
 import { IPost } from '@/app/list/page';
 import { connectDB } from '@/util/database';
 import { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcrypt';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const client = await connectDB;
-  const db = client.db('forum');
-  db.collection('user').insertOne({
-    id: 'id',
-    passwords: 'pass',
-  });
+  if (req.method === 'POST') {
+    const hashedPW = await bcrypt.hash(req.body.password, 10);
+
+    // db에서 이메일 중복 찾아서 return해도 ㄱㅊ
+    const client = await connectDB;
+    const db = client.db('forum');
+    db.collection('user_cred').insertOne({ ...req.body, password: hashedPW });
+  }
 
   return res.status(200).json('가입완료');
 }
