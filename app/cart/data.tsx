f0e74df1ac353b7984472f136be683ec049be48a
bigwave-cho <hooyaa93@gmail.com>
2023-05-21 14:52:23 +0900

    use client(SC, CC)

---
+++ app/cart/data.tsx
@@ -0,0 +1,4 @@
+let age = 20;
+let name = 'ccc';
+
+export { age, name };
--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,7 +1,17 @@
+'use client';
+import { useEffect } from 'react';
+import { age, name } from './data';
+import Example from '@/components/example';
+
 export default function Cart() {
+  useEffect(() => {
+    console.log(age);
+    console.log(name);
+  }, []);
   return (
     <div>
       <h4 className="title">Cart</h4>
+      <Example />
       <div className="cart-item">
         <p>상품명</p>
         <p>$40</p>
