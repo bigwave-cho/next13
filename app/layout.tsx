19cd8a430295e62fed2e0311ca89d64ea2c224a7
bigwave-cho <hooyaa93@gmail.com>
2023-05-21 15:34:38 +0900

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <div className="navbar">
-          <Link href="/">home</Link>
-          <Link href="/list">list페이지</Link>
-        </div>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import 이미지 from '@/public/image.jpg';
-
 export default function Home() {
-  return (
-    <main>
-      {/* <Image src={이미지} alt="이미지" /> */}
-      <Image
-        src="https://images.unsplash.com/photo-1684444161762-95fbabc9a766?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3257&q=80"
-        alt="이미지2"
-        width={100}
-        height={100}
-      />
-      <h1 className="title">Programming Log</h1>
-      <p className="title-sub">by dev kim</p>
-    </main>
-  );
+  return <div>hihi</div>;
 }
